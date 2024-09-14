9f3b92d87ba25bb06653c7cde09653220179c910
chopancho <pancho.pantoja@outlook.es>
2024-09-14 12:29:14 -0300

    wip: correccion controlador y servicio de cursos, ahora se llama cursacion

--- backend/src/cursos/cursos.service.ts
+++ backend/src/cursos/cursos.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Cursacion } from '@prisma/client';
 
 @Injectable()
 export class CursosService {
   constructor(private prisma: PrismaService) {}
 
+  //proximamente desaparecerá...
   async getCursosDeAsignatura(idAsig: number) {
-    return this.prisma.curso.findMany({
+    return this.prisma.cursacion.findMany({
       where: {
         idAsignatura: idAsig,
       },
+    });
+  }
+
+  async getCursacionesDeAsignatura(
+    idAsignatura: number,
+    idPlan,
+  ): Promise<Cursacion[]> {
+    return this.prisma.cursacion.findMany({
+      where: { idPlan: idPlan, idAsignatura: idAsignatura },
     });
   }
 }
