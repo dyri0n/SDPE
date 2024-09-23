ce151e3d527c6ec3828265d9ba3d86dacd78b5a1
PatricioChang <patriciochang12@gmail.com>
2024-09-23 17:10:41 -0300

    Squashed commit of the following:
    commit a56bf9002ed505bf180f204fdf5f4b872b8f8a60
    Author: Daniel <danielalday4@gmail.com>
    Date:   Mon Sep 23 15:54:31 2024 -0300
    
        SPE-10 (SPE-28) SPE-06 (SPE-43) build
    
    commit f761d8f12defa6cf3bf1f9b5736bc1cf1fbc16f7
    Author: Daniel <danielalday4@gmail.com>
    Date:   Fri Sep 6 18:59:30 2024 -0400
    
        creacion componente semestre

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { FluxogramasComponent } from './components/fluxogramas/fluxogramas.component';
 import { CursosComponent } from './components/cursos/cursos.component';
+import { SemestresComponent } from './components/semestres/semestres.component';
+import { AprobacionCursoComponent } from './components/aprobacion-curso/aprobacion-curso.component';
 
 export const routes: Routes = [
     {path: 'fluxogramas', component: FluxogramasComponent},
     {path: 'cursos', component: CursosComponent},
+    {path: 'semestres', component: SemestresComponent},
+    {path: 'aprobacion', component: AprobacionCursoComponent},
     {path: '', redirectTo: '/cursos', pathMatch: 'full'},
     {path: '**', redirectTo: '/cursos'} 
 ];
