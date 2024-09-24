29715ae36bf05c37072b9c6a505f79d4601ce99a
Drzwy <tomassilva0360@gmail.com>
2024-09-24 19:38:53 -0300

    remove: asignatura contemplada module

--- backend/src/asignaturas/asignaturas.service.ts
+++ backend/src/asignaturas/asignaturas.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import {
   AREA,
@@ -53,6 +53,7 @@ export class AsignaturasService {
     });
     return result;
   }
+
   async getSiguienteDeAsignatura(asignaturaId: number): Promise<Tributacion[]> {
     return this.prisma.tributacion.findMany({
       where: { idAsignaturaRequerida: asignaturaId },
