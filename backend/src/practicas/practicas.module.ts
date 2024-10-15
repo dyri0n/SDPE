caa94ad3ec5ae0fd5e15fa01eb04f04c1e9fd6df
chopancho <pancho.pantoja@outlook.es>
2024-10-15 08:51:20 -0300

    Squashed commit from backend

--- backend/src/practicas/practicas.module.ts
+++ backend/src/practicas/practicas.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PracticasController } from './practicas.controller';
 import { PracticasService } from './practicas.service';
+import { EstudiantesService } from '../estudiantes/estudiantes.service';
 
 @Module({
   controllers: [PracticasController],
-  providers: [PracticasService]
+  providers: [PracticasService, EstudiantesService],
 })
 export class PracticasModule {}
