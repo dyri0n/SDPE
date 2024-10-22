fe796dd795e129ff17af3ecb7e702e759d79d54c
Daniel <danielalday4@gmail.com>
2024-10-22 09:11:58 -0300

    fix: vista avance y practicas de un estudiante

--- frontend/src/app/components/practicas-estudiante/practicas-estudiante.component.ts
+++ frontend/src/app/components/practicas-estudiante/practicas-estudiante.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { AlumnoService } from '../../services/alumno.service';
 import { CommonModule } from '@angular/common';
 import { DetallesPracticaDTO } from '../../models/practica';
-
+import { AccordionModule } from 'primeng/accordion';
+import { InfoPracticaDTO } from '../../models/practica';
 @Component({
   selector: 'app-practicas-estudiante',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AccordionModule],
   templateUrl: './practicas-estudiante.component.html',
   styleUrl: './practicas-estudiante.component.css',
 })
@@ -14,7 +15,7 @@ export class PracticasEstudianteComponent {
   // Fixme : Cambiar por id pasada por la vista anterior
   id_estudiante: number = 1;
   practicas_estudiante: DetallesPracticaDTO = new DetallesPracticaDTO();
-
+  tiposPracticas: Practicass[] = [];
   constructor(private readonly alumnoService: AlumnoService) {}
 
   // Antes que se carge el componente
@@ -24,5 +25,29 @@ export class PracticasEstudianteComponent {
       .subscribe((request) => {
         this.practicas_estudiante = request;
       });
+
+    this.tiposPracticas = this.getMatrizDePracticas();
+    console.log(this.tiposPracticas);
   }
+
+  getMatrizDePracticas() {
+    let tipoPractica: Practicass[] = [];
+    this.practicas_estudiante.practicas.forEach((practica) => {
+      let practicasTipo: Practicass = {
+        practicas: [],
+        tipoPractica: practica.posicionRelativa,
+      };
+      this.practicas_estudiante.practicas.forEach((p) => {
+        if (p.posicionRelativa === practicasTipo.tipoPractica) {
+          practicasTipo.practicas.push(p);
+        }
+      });
+      tipoPractica.push(practicasTipo);
+    });
+    return tipoPractica;
+  }
+}
+interface Practicass {
+  tipoPractica: number;
+  practicas: InfoPracticaDTO[];
 }
