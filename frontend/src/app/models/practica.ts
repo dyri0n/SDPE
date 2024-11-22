57828046d3163d59ce316f817de16e8335f137bf
PatricioChang <patriciochang12@gmail.com>
2024-11-21 22:00:54 -0300

    SPE-135 (SPE-75) Ready
    Se hizo la integracion con el backend para poder reflejar los datos del backend en el frontend, se implemento una paginacion para las practicas y se implemento la barra de busqueda.

--- frontend/src/app/models/practica.ts
+++ frontend/src/app/models/practica.ts
@@ -14,3 +14,17 @@ export class DetallesPracticaDTO {
   estudiante: Estudiante = new Estudiante();
   practicas: InfoPracticaDTO[] = [];
 }
+
+export interface PracticaEnConvenioDTO {
+  nombreCompleto: string;
+  tituloPractica: string;
+  numeroPractica: number;
+  fechaInicio: Date;
+  fechaFin: Date;
+  notaFinal: number;
+}
+
+export interface ListarPracticasPorConvenioDTO {
+  tituloConvenio: string;
+  practicas: PracticaEnConvenioDTO[];
+}
