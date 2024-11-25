4dea2eff8a7ea1be02e78360be5126f3bb4ed8e8
chopancho <pancho.pantoja@outlook.es>
2024-11-25 14:35:28 -0300

    test: modulo para agregar resultados de END (falta migrar prisma para crear las tablas en la bd)

--- backend/src/ends/ends.service.ts
+++ backend/src/ends/ends.service.ts
@@ -1,5 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { DocumentoEND } from '@prisma/client';
+import { PatchResultadoENDDTO, PostResultadoENDDTO } from './dto/documento.dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class EndsService {
@@ -30,4 +37,65 @@ export class EndsService {
       },
     });
   }
+
+  //BLOQUE Documentos de END
+  async getDocumentosEND(): Promise<DocumentoEND[]> {
+    return this.prisma.documentoEND.findMany();
+  }
+
+  async getDocumentoById(idDato: number): Promise<DocumentoEND> {
+    return this.prisma.documentoEND.findUnique({
+      where: { idDato: idDato },
+    });
+  }
+  async registrarNuevoResultadoEND(
+    createDto: PostResultadoENDDTO,
+    pathDocumento: string,
+  ) {
+    try {
+      return this.prisma.documentoEND.create({
+        data: {
+          rutaDocumento: pathDocumento,
+          cohorteAsociado: createDto.cohorteAsociado,
+          agnoRendicion: createDto.agnoRendicion,
+          fechaSubida: new Date(),
+        },
+      });
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        // unique constraint
+        if (error.code === 'P2002') {
+          console.error(error);
+          throw new ForbiddenException('Información Duplicada');
+        }
+      } else {
+        throw error;
+      }
+    }
+  }
+  actualizarDocumentoEND(
+    updateDto: PatchResultadoENDDTO,
+    pathDocumento: string,
+  ) {
+    try {
+      return this.prisma.documentoEND.update({
+        where: { idDato: updateDto.idDato },
+        data: {
+          rutaDocumento: pathDocumento,
+          cohorteAsociado: updateDto.cohorteAsociado,
+          agnoRendicion: updateDto.agnoRendicion,
+          fechaSubida: new Date(),
+        },
+      });
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        console.error(error);
+        throw new BadRequestException('Error en los datos de la query');
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  //FIN BLOQUE Documentos END
 }
